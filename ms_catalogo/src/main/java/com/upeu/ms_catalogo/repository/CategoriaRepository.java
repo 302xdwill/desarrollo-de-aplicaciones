5597a78fcaa326250c24183c07b070f0037b93c9
302xdwill <davidom302@gmail.com>
2025-03-27 00:41:12 -0500

    catalogo

---
+++ ms_catalogo/src/main/java/com/upeu/ms_catalogo/repository/CategoriaRepository.java
@@ -0,0 +1,7 @@
+package com.upeu.ms_catalogo.repository;
+
+import com.upeu.ms_catalogo.entity.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
