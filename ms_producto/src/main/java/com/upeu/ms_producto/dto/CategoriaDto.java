19f0b1752e4043fc4193ca5913b328e6b4e38e9e
302xdwill <davidom302@gmail.com>
2025-05-04 22:30:24 -0500

    resilencia

--- ms_producto/src/main/java/com/upeu/ms_producto/dto/CategoriaDto.java
+++ ms_producto/src/main/java/com/upeu/ms_producto/dto/CategoriaDto.java
@@ -4,6 +4,40 @@ import lombok.Data;
 
 @Data
 public class CategoriaDto {
-    private Integer id;
     private String nombre;
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public CategoriaDto(String nombre, Integer id) {
+        this.nombre = nombre;
+        this.id = id;
+    }
+
+    public CategoriaDto() {
+    }
+
+    @Override
+    public String toString() {
+        return "CategoriaDto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
+
+
 }
